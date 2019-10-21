b981b2ab03254d67529ab61b51d60a8d3d85c67f
Santosh Srinivas <santosh.srinivas@gmail.com>
2019-10-21 12:22:54 +0530

    add substring helpers

--- string-helpers.js
+++ string-helpers.js
@@ -1,5 +1,4 @@
 import { Template } from 'meteor/templating';
-import { s } from 'meteor/underscorestring:underscore.string';
 
 Template.helpers({
 	
@@ -13,6 +12,10 @@ Template.helpers({
 	
 	concat(...rest) {
 		return rest.join('');
-	}
-	
+	},
+
+	substring(string, start, n) {
+		// credit: https://github.com/epeli/underscore.string#sstring--chain
+		return s(string).substring(start, n).value();
+	},
 });
